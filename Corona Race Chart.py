2f0b9300af831423208c5dd6fc038481292f4956
Kshitij2727 <Kshitijjain2727@gmail.com>
2020-05-19 14:58:54 +0530

    Kshitij COVID19 Project

--- Corona Race Chart.py
+++ Corona Race Chart.py
@@ -8,11 +8,10 @@ grouped = df1.groupby(['location','date'])
 df1_confirmed = grouped.sum().reset_index().sort_values(['date'],ascending=True)
 df11_confirmed=df1_confirmed.head(12)
 print(df1_confirmed)
+
 #Initial Bar Chart Of Max cases stored in df11_confirmed
 fig, ax = plt.subplots(figsize=(15, 8))
-#ax.barh(df11_confirmed['location'], df11_confirmed['total_cases'])
 ax.barh(df1_confirmed['location'], df1_confirmed['total_cases'])
-
 plt.xlabel('Number of Confirmed Cases')
 plt.ylabel('location')
 dff=df11_confirmed[::-1]
@@ -20,7 +19,7 @@ fig, ax = plt.subplots(figsize=(15, 8))
 ax.barh(dff['location'], dff['total_cases'])
 plt.xlabel('Number of Confirmed Cases')
 plt.ylabel('location')
-#plt.show()
+
 #Code for individual colors for each country using dictionary
 from random import randint
 import random
@@ -30,21 +29,22 @@ for i in range(len(df1_confirmed.location.unique())):
     c_code.append('#%06X' % randint(0, 0xFFFFFF))
 
 colors = dict(zip(df1_confirmed.location.unique(), c_code))
-#first step
+
+#First step
 fig, ax = plt.subplots(figsize=(15, 8))
-# pass colors values to `color=`
+
+#Pass colors values to `color=`
 ax.barh(dff['location'], dff['total_cases'], color=[colors[x] for x in dff['location']])
-# iterate over the values to plot labels and values
+
+#Iterating over the values to plot labels and values
 for i, (value, name) in enumerate(zip(dff['total_cases'], dff['location'])):
-    ax.text(value,i      ,name,ha='right')  # Tokyo: name
-    ax.text(value,i-0.25 ,value,ha='right')   # 38194.2: value
-# Add year right middle portion of canvas
+    ax.text(value,i      ,name,ha='right')
+    ax.text(value,i-0.25 ,value,ha='right')
+
+#Add year right middle portion of canvas
 ax.text(1, 0.4, "17/05/2020", transform=ax.transAxes, size=25, ha='right')
-#plt.show()
 
-
-#Function Code Main
-
+#Main Function Code
 fig, ax = plt.subplots(figsize=(15, 8))
 def draw_racechart(date,case="total_cases"):
     dff= (df1_confirmed[df1_confirmed['date'].eq(date)].sort_values(by=case,ascending=False).head(10))[::-1]
@@ -54,10 +54,9 @@ def draw_racechart(date,case="total_cases"):
     for i, (value, name) in enumerate(zip(dff[case], dff['location'])):
         ax.text(value-dx, i,     name,           size=14, weight=600, ha='right', va='bottom')
         ax.text(value-dx, i-.25, value, size=10, color='#17202A', ha='right', va='baseline')
-        #ax.text(value+dx, i,     f'{value:,.0f}',  size=14, ha='left',  va='center')
-    # ... polished styles
+
+    #Adding more styles
     ax.text(1, 0.4, date, transform=ax.transAxes, color='#777777', size=30, ha='right', weight=800)
-    #ax.text(0, 1.06, 'Number of Cases', transform=ax.transAxes, size=12, color='#777777')
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
     ax.xaxis.set_ticks_position('top')
     ax.tick_params(axis='x', colors='#bf1d25', labelsize=12)
@@ -74,7 +73,7 @@ def draw_racechart(date,case="total_cases"):
 
 draw_racechart("2020-05-17")
 
-#Final Animation
+#Animation
 from matplotlib import animation as Anim
 from IPython.display import HTML
 fig,ax = plt.subplots(figsize=(16,10)) #Set figure for plot
